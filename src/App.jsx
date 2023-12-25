93a6cc4fa064b08e53c66fbb41f797c5fc9f16b5
Panupong <Pai28212m@gmail.com>
2023-12-26 00:03:55 +0700

    HH:Pai

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchText, setSearchText] = useState("Adobe");
+  const [booklist, setBookList] = useState([]);
+
+  useEffect(() => {
+    getBookName();
+  }, [searchText]);
+
+  async function getBookName() {
+    const getBookUrl =
+      "https://www.googleapis.com/books/v1/volumes?q=" + searchText;
+    const respond = await axios.get(getBookUrl);
+    setBookList(respond.data.items);
+  }
+
+  return (
+    <div className="App">
+      <h3>Find a Book</h3>
+      <input
+        onChange={(event) => setSearchText(event.target.value)}
+        value={searchText}
+      ></input>
+
+    <ul>
+      {booklist.map((item) => {
+          return(
+
+            <li>{item.volumeInfo.title}</li>
+          )
+
+
+      })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
