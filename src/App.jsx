62802f99317e0008c6492a4d6a4a79758709a427
KIATTISAK PIMARPORN <enterpannet@gmail.com>
2023-12-26 11:46:57 +0700

    fear:search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
 import "./App.css";
-
+import axios from "axios";
+import { useEffect } from "react";
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [books, setBooks] = useState("");
+  const [showBook, setShowBooks] = useState([]);
+  const getBooks = async () => {
+    try {
+      const result = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${books}`
+      );
+
+      setShowBooks(result.data.items);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (books === "") {
+      setShowBooks([]);
+    } else {
+      getBooks();
+    }
+  }, [books]);
+  return (
+    <>
+      <div className="App">
+        <p>Find A Book</p>
+        <input
+          type="text"
+          value={books}
+          onChange={(e) => {
+            setBooks(e.target.value);
+          }}
+        />
+        <div className="showBook">
+          <ul>
+            {showBook
+              ? showBook.map((item) => {
+                  return <li key={item.id}>{item.volumeInfo.title}</li>;
+                })
+              : ""}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
